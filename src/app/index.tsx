6be0b3d8cdc80fc17662c27e5e612ae79e25dcd3
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2018-05-30 00:09:21 +0530

    Layout defined

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import {Route, Redirect, Switch} from 'react-router-dom';
 import Login from './login';
+import MiniDrawer from './layout';
 
 const NoMatch = () => (
   <h1 style={{color:'red'}}>Page not found!</h1>
@@ -14,6 +15,7 @@ export class App extends React.Component<object, object> {
         <div>
           <Switch>
             <Route exact path='/login' component={Login} />
+            <Route exact path='/layout' component={MiniDrawer} />
             <Redirect exact from='/' to='login' />
             <Route component={NoMatch}/>
           </Switch>
