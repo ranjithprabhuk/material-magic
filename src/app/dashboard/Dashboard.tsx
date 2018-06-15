781ae7585620a4ac39514ddb783077d69fa68954
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2018-06-16 02:03:23 +0530

    temporary removal for redirect

--- src/app/dashboard/Dashboard.tsx
+++ src/app/dashboard/Dashboard.tsx
@@ -1,17 +1,11 @@
 import * as React from 'react';
-import * as classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
-import { Drawer, AppBar, Toolbar, List, Typography, Divider, IconButton } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import { otherMailFolderListItems } from './menuItems';
 import { dashboardStyles } from './dashboard.styles';
 import { IDashboardProps, IDashboardState } from './IDashboard';
 import Home from '../home';
 import Charts from '../charts';
 import Buttons from '../buttons';
-import { Route, Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Sidebar from './SideBar';
 import Header from './Header';
 
@@ -21,7 +15,6 @@ class Dashboard extends React.Component<IDashboardProps, IDashboardState> {
     this.state = {
       open: false,
     };
-    this.handleDrawerClose = this.handleDrawerClose.bind(this);
   }
 
   public handleDrawerOpen = () => {
@@ -30,50 +23,6 @@ class Dashboard extends React.Component<IDashboardProps, IDashboardState> {
 
   public handleDrawerClose = () => {
     this.setState({ open: false });
-  }
-
-  public renderAppHeader = (classes: any) => {
-    return (
-      <AppBar position='absolute' className={classNames(classes.appBar, this.state.open && classes.appBarShift)}>
-        <Toolbar disableGutters={!this.state.open}>
-          <IconButton
-            color='inherit'
-            aria-label='open drawer'
-            onClick={this.handleDrawerOpen}
-            className={classNames(classes.menuButton, this.state.open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant='title' color='inherit' noWrap className={classNames(this.state.open && classes.hide)}>
-            Material Magic
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    );
-  }
-
-  public renderSidebar = (classes: any) => {
-    return (
-      <Drawer
-        variant='permanent'
-        classes={{
-          paper: classNames(classes.drawerPaper, !this.state.open && classes.drawerPaperClose),
-        }}
-        open={this.state.open}
-      >
-        <div className={classes.toolbar}>
-          <Typography variant='title' color='primary' noWrap>
-            Material Magic
-          </Typography>
-          <IconButton onClick={this.handleDrawerClose}>
-            {this.props.theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
-        </div>
-        <Divider />
-        <Divider />
-        <List>{otherMailFolderListItems}</List>
-      </Drawer>
-    );
   }
 
   public render(): React.ReactElement<Dashboard> {
@@ -89,7 +38,6 @@ class Dashboard extends React.Component<IDashboardProps, IDashboardState> {
               <Route exact path='/dashboard/home' component={Home} />
               <Route exact path='/dashboard/charts' component={Charts} />
               <Route exact path='/dashboard/buttons' component={Buttons} />
-              <Redirect exact from='/' to='/dashboard/home' />
             </div>
           </div>
         </main>
