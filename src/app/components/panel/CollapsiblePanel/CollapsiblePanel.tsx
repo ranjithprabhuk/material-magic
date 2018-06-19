417501c8c6b3e5b0b08c202459ab8ab54da87c31
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2018-06-19 20:13:13 +0530

    removed un used modules

--- src/app/components/panel/CollapsiblePanel/CollapsiblePanel.tsx
+++ src/app/components/panel/CollapsiblePanel/CollapsiblePanel.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import { withStyles, Theme } from '@material-ui/core/styles';
 import * as classnames from 'classnames';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
