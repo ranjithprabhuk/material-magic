2549151b3e57f8419bdccf96d523097ce248f2f3
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2018-06-25 15:22:53 +0530

    custom header added to the Collapsible panel

--- src/app/components/panel/CollapsiblePanel/CollapsiblePanel.tsx
+++ src/app/components/panel/CollapsiblePanel/CollapsiblePanel.tsx
@@ -5,7 +5,6 @@ import {Card, Typography} from '@material-ui/core';
 import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
-import FavoriteIcon from '@material-ui/icons/Favorite';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const styles = (theme: Theme) => ({
@@ -33,23 +32,20 @@ class CollapsiblePanel extends React.Component<any, any> {
   }
 
   public render(): any {
-    const { classes, header } = this.props;
+    const { classes, header, customheader } = this.props;
 
     return (
       <div>
         <Card>
           <CardActions className={classes.actions} disableActionSpacing>
-            <IconButton aria-label='Add to favorites'>
-              <FavoriteIcon />
-            </IconButton>
-            <Typography paragraph variant='body2'>{header}</Typography>
+            {customheader ? customheader : <Typography variant='body2'>{header}</Typography>}
             <IconButton
               className={classnames(classes.expand, {
                 [classes.expandOpen]: this.state.expanded,
               })}
               onClick={this.handleExpandClick}
               aria-expanded={this.state.expanded}
-              aria-label='Show more'
+              aria-label={this.state.expand ? 'Hide' : 'Show More'}
             >
               <ExpandMoreIcon />
             </IconButton>
