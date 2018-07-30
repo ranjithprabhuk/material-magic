4b95824d44dbdecb8e0ae1330b233e24927a81aa
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2018-07-31 01:59:57 +0530

    Settings changes

--- src/app/dashboard/SideBar/Sidebar.tsx
+++ src/app/dashboard/SideBar/Sidebar.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
 import * as classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
-import { Icon, Typography } from '@material-ui/core';
+import { IconButton, Typography } from '@material-ui/core';
 import { sideBarStyles } from './sideBar.styles';
 import Close from '@material-ui/icons/Close';
 import { ISideBarProps, ISideBarState } from './ISideBar';
 import UserProfile from '../../user-profile';
+import Settings from '../../settings';
 
 class SideBar extends React.Component<ISideBarProps, ISideBarState> {
   public renderViewContent(content: string): React.ReactNode {
     switch(content) {
       case 'User Profile': return (<UserProfile />);
+      case 'Settings': return (<Settings />);
       default: return null;
     }
   }
@@ -22,9 +24,9 @@ class SideBar extends React.Component<ISideBarProps, ISideBarState> {
       <div className={classNames(classes.sideBar, isSideBarOpen && classes.sideBarOpen)}>
         <div className={classes.sideBarHeader}>
           <Typography variant='title' color='textSecondary'>{viewContent}</Typography>
-          <Icon className={classes.closeIcon} color='action'>
+          <IconButton>
             <Close onClick={() => toggleSideBar()} />
-          </Icon>
+          </IconButton>
         </div>
           {this.renderViewContent(viewContent)}
       </div>
