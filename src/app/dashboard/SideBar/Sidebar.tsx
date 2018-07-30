54d1fed31836d5e13855687e18a8d1432fe04ff9
Ranjithprabhu <ranjithprabhu.k@gmail.com>
2018-07-30 20:05:55 +0530

    Sidebar style changes

--- src/app/dashboard/SideBar/Sidebar.tsx
+++ src/app/dashboard/SideBar/Sidebar.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import * as classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
+import { Icon, Typography } from '@material-ui/core';
 import { sideBarStyles } from './sideBar.styles';
+import Close from '@material-ui/icons/Close';
 import { ISideBarProps, ISideBarState } from './ISideBar';
 import UserProfile from '../../user-profile';
 
@@ -13,10 +15,14 @@ class SideBar extends React.Component<ISideBarProps, ISideBarState> {
     }
   }
   public render(): React.ReactElement<SideBar> {
-    const { classes, isSideBarOpen } = this.props;
+    const { classes, isSideBarOpen, toggleSideBar } = this.props;
 
     return (
       <div className={classNames(classes.sideBar, isSideBarOpen && classes.sideBarOpen)}>
+        <div className={classes.sideBarHeader}>
+          <Typography variant='title' color='textSecondary'>Header</Typography>
+          <Icon color='action'><Close onClick={() => toggleSideBar()} /></Icon>
+        </div>
           {this.renderViewContent()}
       </div>
     );
--- src/app/dashboard/SideBar/sideBar.styles.tsx
+++ src/app/dashboard/SideBar/sideBar.styles.tsx
@@ -4,16 +4,22 @@ import { ThemeConfig } from '../../../theme';
 export const sideBarStyles = (theme: Theme): any => ({
   sideBar: {
     display: 'flex',
+    flexDirection: 'column',
     width: ThemeConfig.sideBarWidth,
     marginRight: -ThemeConfig.sideBarWidth,
     transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
-    background: theme.palette.primary.main,
+    background: theme.palette.primary.dark,
   },
   sideBarOpen: {
     marginRight: 0,
     padding: theme.spacing.unit,
+  },
+  sideBarHeader: {
+    display: 'flex',
+    margin: theme.spacing.unit * 1.25,
+    justifyContent: 'space-between',
   },
 });
